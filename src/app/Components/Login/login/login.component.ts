914986e63dfc3c222783c31407aa35ed9134f8e9
Nikhil Dethe <dethenikhil7578@gmail.com>
2023-03-30 11:59:41 +0530

    Add login page

--- src/app/Components/Login/login/login.component.ts
+++ src/app/Components/Login/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/Auth/AuthService';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
- 
+  username!: string;
+  password!: string;
+  errorMessage = 'Invalid Credentials';
+  successMessage!: string;
+  invalidLogin = false;
+  loginSuccess = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+  }
+
+  handleLogin() {
+    this.authService.login(this.username, this.password).subscribe((result) => {
+      this.invalidLogin = false;
+      this.loginSuccess = true;
+      this.successMessage = 'Login Successful';
+      // this.router.navigate(['/home']);
+    }, () => {
+      this.invalidLogin = true;
+      this.loginSuccess = false;
+    });
+  }
+
 }
