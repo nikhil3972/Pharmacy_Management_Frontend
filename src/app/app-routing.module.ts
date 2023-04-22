cf38f550a1aa57bad0e5c1c366c5cf7ce92e0120
Nikhil Dethe <dethenikhil7578@gmail.com>
2023-04-23 02:32:52 +0530

    Navbar is display only after login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CustomerComponent } from './Components/Customer/customer/customer.component';
 import { DashboardComponent } from './Components/Dashboard/dashboard/dashboard.component';
 import { DiseaseComponent } from './Components/disease/disease.component';
-import { LoginComponent } from './Components/Login/login/login.component';
 import { ProcureComponent } from './Components/Procuree/procure/procure.component';
 import { SalesComponent } from './Components/Sales/sales.component';
 
 const routes: Routes = [
-  {path : '', component : LoginComponent},
   {path : 'dashboard', component : DashboardComponent},
   {path : 'customer', component : CustomerComponent},
   {path: 'procure', component:ProcureComponent},
